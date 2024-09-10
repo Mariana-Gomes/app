9650e1a2dffd9e8d49c38611754c0b3b2031f224
Mariana Gomes <marianagds.dg@gmail.com>
2024-09-10 16:25:03 -0300

    inicia o menu da aplicação

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const start = () => {
+const { select } = require("@inquirer/prompts");
+
+const start = async () => {
   while (true) {
-    let opcao = "sair";
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "Listar metas",
+          value: "listar",
+        },
+        {
+          name: "Sair",
+          value: "sair",
+        },
+      ],
+    });
+
     switch (opcao) {
       case "cadastrar":
         console.log("Vamos cadastrar");
@@ -9,6 +28,7 @@ const start = () => {
         console.log("Vamos listar");
         break;
       case "sair":
+        console.log("Até a próxima");
         return;
     }
   }
