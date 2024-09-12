6b9a4fe1aff52d6453decd908d8897309fb592cc
Mariana Gomes <marianagds.dg@gmail.com>
2024-09-12 17:54:32 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -56,8 +56,24 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "Metas realizadas",
+    message: "Metas realizadas: " + realizadas.length,
     choices: [...realizadas],
+  });
+};
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return !meta.checked;
+  });
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas :)");
+    return;
+  }
+
+  await select({
+    message: "Metas abertas: " + abertas.length,
+    choices: [...abertas],
   });
 };
 
@@ -79,6 +95,10 @@ const start = async () => {
           value: "realizadas",
         },
         {
+          name: "Metas abertas",
+          value: "abertas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -95,6 +115,9 @@ const start = async () => {
         break;
       case "realizadas":
         await metasRealizadas();
+        break;
+      case "abertas":
+        await metasAbertas();
         break;
       case "sair":
         console.log("Até a próxima");
